c384430f600a9d454a550ae42ee30f3d17a6480d
Brendon Willian <bmota@its.jnj.com>
2017-04-10 15:11:35 -0300

    Translation

--- app/core/core.config.js
+++ app/core/core.config.js
@@ -6,11 +6,51 @@
         .module('portal.core')
         .config(config);
 
-    config.$inject = ['cfpLoadingBarProvider'];
+    config.$inject = ['cfpLoadingBarProvider', '$translateProvider'];
 
-    function config(cfpLoadingBarProvider)
+    function config(cfpLoadingBarProvider, $translateProvider)
     {
-        cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
-        cfpLoadingBarProvider.spinnerTemplate = '<div id="loading-bar-spinner"><div class="spinner-icon"></div></div>';
+        function _init()
+        {
+            cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
+            cfpLoadingBarProvider.spinnerTemplate = '<div id="loading-bar-spinner"><div class="spinner-icon"></div></div>';
+            setInternalization();
+        }
+
+        _init();
+
+        function setInternalization()
+        {
+            $translateProvider.useSanitizeValueStrategy(null);
+            $translateProvider.translations('en', {
+                HOME         : 'Home',
+                USER_SETTINGS: 'User Settings',
+                MY_PROFILE   : 'My Profile',
+                OTHERS       : 'Others',
+                LOG_OUT      : 'Log out',
+            });
+            $translateProvider.translations('pt', {
+                HOME         : 'Início',
+                USER_SETTINGS: 'Configuração de Usuario',
+                MY_PROFILE   : 'Meu usuario',
+                OTHERS       : 'Outros',
+                LOG_OUT      : 'Sair',
+            });
+            $translateProvider.translations('es', {
+                HOME         : 'Principal',
+                USER_SETTINGS: 'Configuración de usuario',
+                MY_PROFILE   : 'Mi perfil',
+                OTHERS       : 'Otros',
+                LOG_OUT      : 'Cerrar sesión',
+            });
+
+            let language = localStorage.getItem('language');
+
+            if (language === null)
+            {
+                language = 'en';
+            }
+            $translateProvider.preferredLanguage(language);
+        }
     }
 })();
